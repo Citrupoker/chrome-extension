6991aab2b54e3bd90f38c5b7068e870870929670
hamzicabdulah <hamzic@live.com>
2017-07-30 22:30:05 +0200

    Got recaptcha solving process working and notifying user

---
+++ content.js
@@ -0,0 +1,56 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var captchaDiv = $('#recaptcha-demo')
+
+    // Add an alert above the captcha div to let the user know their solution is on its way
+    $('<h2>Captcha being solved. Please wait.</h2>').insertBefore(captchaDiv);
+    captchaDiv.prev('h2').addClass('notification');
+
+    var token = request.token;
+    var url = document.URL;
+
+    if (captchaDiv.length > 0) {
+        // If there actually is a recaptcha on the page, get the solution and insert it in the appropriate textarea
+        var recaptchaKey = captchaDiv.attr('data-sitekey');
+
+        getSolution(token, url, recaptchaKey, function(solution) {
+            $('#g-recaptcha-response').val(solution);
+            $('.notification').html('Captcha is solved. You can now submit your form.');
+        });
+    } else 
+        // There is not recaptcha on the currently opened tab
+        alert( "Something went wrong. It looks like you're on the wrong page.");
+});
+
+function getSolution(token, url, key, callback) {
+  // Uses anticaptcha library and returns solution
+  var anticaptcha = Anticaptcha(token);
+  anticaptcha.setWebsiteURL(url);
+  anticaptcha.setWebsiteKey(key);
+  anticaptcha.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116");
+  
+  anticaptcha.getBalance(function (err, balance) {
+      if (err) {
+          console.error(err);
+          return;
+      }
+      if (balance > 0) {
+          anticaptcha.createTaskProxyless(function (err, taskId) {
+              if (err) {
+                  console.error(err);
+                  return;
+              }
+  
+              console.log(taskId);
+  
+              anticaptcha.getTaskSolution(taskId, function (err, taskSolution) {
+                  if (err) {
+                      console.error(err);
+                      return;
+                  }
+  
+                  callback(taskSolution);
+              });
+          });
+      }
+  });
+}
--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-//
+$(document).ready(function() {
+    $('.solveBtn').click(function() {
+        // Send the token to the content script when the solve button is pressed
+        var anticaptchaToken = $('.token').val();
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {token: anticaptchaToken});
+        });
+    });
+});
