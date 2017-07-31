b8fd653117a049fd81b62e2117a452407ddebe2e
hamzicabdulah <hamzic@live.com>
2017-07-31 14:52:49 +0200

    Added submit after solving checkbox

--- content.js
+++ content.js
@@ -1,32 +1,46 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var captchaDiv = $('#recaptcha-demo')
+    var captchaDiv = $('#recaptcha-demo');
     var token = request.token;
     var url = document.URL;
 
-    if(token){
-        localStorage.setItem('token', token);
+    if (token) {
         // Add an alert above the captcha div to let the user know their solution is on its way
-        $('<h2 style="border:1px solid red;border-radius:5px;text-align: center;padding:6px;">Captcha being solved. Please wait.</h2>').insertBefore(captchaDiv);
-        captchaDiv.prev('h2').addClass('notification');
-    }else {
-        $('<h2 style="border:1px solid yellow;border-radius:5px;text-align: center;padding:6px;">Please provide a token</h2>').insertBefore(captchaDiv);
+        if ($('.notification').length > 0) {
+            $('.notification').html('Captcha being solved. Please wait.');
+        } else {
+            $('<h2 style="border:1px solid red;border-radius:5px;text-align: center;padding:6px;">Captcha being solved. Please wait.</h2>').insertBefore(captchaDiv);
+            captchaDiv.prev('h2').addClass('notification');
+        }
+
+        if (captchaDiv.length > 0) {
+            // If there actually is a recaptcha on the page, get the solution and insert it in the appropriate textarea
+            var recaptchaKey = captchaDiv.attr('data-sitekey');
+
+            getSolution(token, url, recaptchaKey, function(solution, invalidToken) {
+                if (solution) {
+                    $('#g-recaptcha-response').val(solution);
+                    if (request.submit)
+                        $('#recaptcha-demo-submit').click();
+                    else 
+                        $('.notification').html('Captcha is solved. You can now submit your form.');
+                } else if (invalidToken) {
+                    $('.notification').html('The token you have provided is invalid. Please try again.');
+                }
+            });
+        } else if ($('.notification').length > 0) {
+            // There is no recaptcha on the currently opened tab
+            $('.notification').html('Something went wrong. It looks like you\'re on the wrong page.');
+        } else {
+            $('body').prepend('<h2 style="border:1px solid red;border-radius:5px;text-align: center;padding:6px;">Something went wrong. It looks like you\'re on the wrong page.</h2>');            
+            $('h2').addClass('notification');
+        }
+    } else if ($('.notification').length > 0) {
+        // No token is provided
+        $('.notification').html('Please provide a token.');
+    } else {
+        $('<h2 style="border:1px solid yellow;border-radius:5px;text-align: center;padding:6px;">Please provide a token.</h2>').insertBefore(captchaDiv);
         captchaDiv.prev('h2').addClass('notification');
     }
-
-
-
-
-    if (captchaDiv.length > 0) {
-        // If there actually is a recaptcha on the page, get the solution and insert it in the appropriate textarea
-        var recaptchaKey = captchaDiv.attr('data-sitekey');
-
-        getSolution(token, url, recaptchaKey, function(solution) {
-            $('#g-recaptcha-response').val(solution);
-            $('.notification').html('Captcha is solved. You can now submit your form.');
-        });
-    } else 
-        // There is not recaptcha on the currently opened tab
-        alert( "Something went wrong. It looks like you're on the wrong page.");
 });
 
 function getSolution(token, url, key, callback) {
@@ -39,7 +53,7 @@ function getSolution(token, url, key, callback) {
   anticaptcha.getBalance(function (err, balance) {
       if (err) {
           console.error(err);
-          return;
+          callback(null, true);
       }
       if (balance > 0) {
           anticaptcha.createTaskProxyless(function (err, taskId) {
--- popup.js
+++ popup.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-    $('.solveBtn').click(function() {
+    var solve = function() {
+        localStorage.setItem('token', $('.token').val());
         // Send the token to the content script when the solve button is pressed
         var anticaptchaToken = $('.token').val();
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {token: anticaptchaToken});
+            chrome.tabs.sendMessage(tabs[0].id, {token: anticaptchaToken, submit: $('.submit').is(':checked')});
         });
+    }
+
+    // Check for token and checkboxes in local storage to set up on load
+    if (localStorage.getItem('token') !== null)
+        $('.token').val(localStorage.getItem('token'));
+    if (localStorage.getItem('solveOnLoad') == 'true')
+        $('.onload').prop('checked', true);
+    if (localStorage.getItem('submitAfterSolved') == 'true') 
+        $('.submit').prop('checked', true);
+
+    // Store checkbox status in local storage on change
+    $('.onload').change(function() {
+        localStorage.setItem('solveOnLoad', $('.onload').is(':checked'));
     });
+    $('.submit').change(function() {
+        localStorage.setItem('submitAfterSolved', $('.submit').is(':checked'));
+    });
+
+    $('.solveBtn').click(solve);
+
+
 });
